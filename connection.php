df69bc26cee493b262a90e3e48522853653c8ba3
htcuong27 <cuong.htan27@gmail.com>
2016-12-03 18:09:20 +0700

    Delete Cmt

--- connection.php
+++ connection.php
@@ -1,10 +1,9 @@
 <?php
-$host="localhost"; // luôn luôn là localhost
-$username="root"; // user của mysql
-$password=""; // Mysql password
-$db_name="cmiiw"; // tên database
+$host="localhost"; 
+$username="root"; 
+$password=""; 
+$db_name="cmiiw"; 
  
-// kết nối cơ sở dữ liệu
 mysql_connect("$host", "$username", "$password")or die("Không thể kết nối");
 mysql_select_db("$db_name")or die("cannot select DB");
 ?>
